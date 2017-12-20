cc77558472d28fd724d7a2356a02f41c6f410aa9
jobradley18 <34578432+jobradley18@users.noreply.github.com>
2017-12-20 09:52:55 -0500

    Update living_Quarters.py

--- living_Quarters.py
+++ living_Quarters.py
@@ -1,11 +1,8 @@
 While x = 1:
-
-
-input("You are decending in an old elevator. Slowly, it creaks to a stop and the doors open.")
-input("You are greeted with the rancid smell of teen's sweat.")
-input("An errie white lights line the ceiling, a few flicker as you look on")
-input("There are two rows of beds on either side of the room.")
-input("Eight beds in total each with pristine white sheets")
-input("There is a pillow at the head of each bed and a chest at the foot.")
-input("Everything is still, too still...")
-
+    input("You are decending in an old elevator. Slowly, it creaks to a stop and the doors open.")
+    input("You are greeted with the rancid smell of teen's sweat.")
+    input("An errie white lights line the ceiling, a few flicker as you look on")
+    input("There are two rows of beds on either side of the room.")
+    input("Eight beds in total each with pristine white sheets")
+    input("There is a pillow at the head of each bed and a chest at the foot.")
+    input("Everything is still, too still...")
