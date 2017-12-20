90def9dae01361ca202a68ba25984053ab44cfb8
jobradley18 <34578432+jobradley18@users.noreply.github.com>
2017-12-20 10:46:18 -0500

    Update living_Quarters.py

--- living_Quarters.py
+++ living_Quarters.py
@@ -1,3 +1,5 @@
+########################################################
+
 while True:
     input("You are decending in an old elevator. Slowly, it creaks to a stop and the doors open.")
     input("You are greeted with the rancid smell of teen's sweat.")
@@ -6,20 +8,34 @@ while True:
     input("Eight beds in total each with pristine white sheets")
     input("There is a pillow at the head of each bed and a chest at the foot.")
     input("Everything is still, too still...")
-
-
     
-    inventory = ["key"]
+########################################################
 
-    if "key" in inventory: 
+    inventory = [] 
+    input("The bed...")
+    input("Its pressence calls out to you.")
+    input("You notice that something silver underneath the pillow.")
+    while True:
+        investigate = input("Do you want to investigate?:")
+        if investigate.lower() == "yes":       
+            input("You found a KEY")
+            inventory.append("KEY")
+            break
+        elif investigate.lower() == "no":
+            break
+
+########################################################
+
+    trunk = ["NOTEBOOK", "PEN","DAGGER"]
+    if "KEY" in inventory: 
         input("Wth the key in hand you approach the large wooden trunk.")
         input("You insert the key hopefully into the old lock on the front.")
         input("In the trunk you find a collection of NOTEBOOKS, PENS, a  SMALL DAGGER,")
+        inventory.extend(trunk) 
     else:
         input("This trunk requires a KEY")
+   
+########################################################
 
-    trunk = ["Notebook", "Pen","Dagger"]
+    break
     
-    continue
-    
-inventory.extend(trunk)
