3749ee46c62f522947a48496baf3b08fbe3b3285
jobradley18 <34578432+jobradley18@users.noreply.github.com>
2017-12-20 10:09:52 -0500

    Update living_Quarters.py

--- living_Quarters.py
+++ living_Quarters.py
@@ -1,4 +1,4 @@
-While x = 1:
+while True:
     input("You are decending in an old elevator. Slowly, it creaks to a stop and the doors open.")
     input("You are greeted with the rancid smell of teen's sweat.")
     input("An errie white lights line the ceiling, a few flicker as you look on")
@@ -6,3 +6,4 @@ While x = 1:
     input("Eight beds in total each with pristine white sheets")
     input("There is a pillow at the head of each bed and a chest at the foot.")
     input("Everything is still, too still...")
+    continue
